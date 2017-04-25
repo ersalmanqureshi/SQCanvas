5d137c8ea9ae30813edd5164ea4b5c1237b08247
Salman Qureshi <er.salmanqureshi@gmail.com>
2017-04-26 04:54:48 +0530

    Implement bottom launcher in controller

--- SQCanvas/SQCanvas/CanvasViewController.swift
+++ SQCanvas/SQCanvas/CanvasViewController.swift
@@ -13,12 +13,41 @@ class CanvasViewController: UIViewController {
     //MARK: IBOutlet
     @IBOutlet weak var boundaryView: UIView!
     
+    let bottomLauncher = BottomLauncherView()
+    
     //MARK: View controller lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    @IBAction func hanldeLauncher(_ sender: UIBarButtonItem) {
         
+        //Set delegate
+        bottomLauncher.delegate = self
+    }
+    
+    @IBAction func hanldeLauncher(_ sender: UIBarButtonItem) {
+        bottomLauncher.showFloaterView()
     }
 }
+
+extension CanvasViewController: BottomLauncherViewDelegate {
+    func didPresentViewController(_ index: Int) {
+        if index == 0{
+            
+            bottomLauncher.handleDismiss()
+            
+            print("Camera")
+            
+            
+        }else if index == 1{
+            bottomLauncher.handleDismiss()
+            
+            print("Gallery")
+           
+        }else{
+            bottomLauncher.handleDismiss()
+        }
+
+    }
+}
+
+
 
