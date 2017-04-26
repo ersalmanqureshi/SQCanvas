447f773c625bf7b81fb81b2b553f37af5d6a3a03
Salman Qureshi <er.salmanqureshi@gmail.com>
2017-04-26 07:35:28 +0530

    Refactor folder, pan gesture added

--- SQCanvas/CanvasViewController.swift
+++ SQCanvas/CanvasViewController.swift
@@ -15,6 +15,29 @@ class CanvasViewController: UIViewController {
     
     let bottomLauncher = BottomLauncherView()
     
+    lazy var tapGestureRecognizer: UITapGestureRecognizer = { [unowned self] in
+        let gesture = UITapGestureRecognizer()
+        gesture.addTarget(self, action: #selector(overlayViewDidTap(_:)))
+        //gesture.delegate = self
+        return gesture
+    }()
+    
+    lazy var panGestureRecognizer: UIPanGestureRecognizer = { [unowned self] in
+        let gesture = UIPanGestureRecognizer()
+        gesture.addTarget(self, action: #selector(handlePanGesture(_:)))
+        //gesture.delegate = self
+        
+        return gesture
+    }()
+    
+    lazy var rotationGestureRecognizer: UIRotationGestureRecognizer = { [unowned self] in
+        let gesture = UIRotationGestureRecognizer()
+        gesture.addTarget(self, action: #selector(handleRotateGesture(_:)))
+        //gesture.delegate = self
+        
+        return gesture
+    }()
+    
     //MARK: View controller lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +48,62 @@ class CanvasViewController: UIViewController {
     
     @IBAction func hanldeLauncher(_ sender: UIBarButtonItem) {
         bottomLauncher.showFloaterView()
+    }
+    
+    // MARK: - Tap gesture recognizer
+    func overlayViewDidTap(_ tapGestureRecognizer: UITapGestureRecognizer) {
+        
+    }
+    
+    // MARK: - Pan gesture recognizer
+    func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
+        
+        let translation = gesture.translation(in: boundaryView)
+    
+        gesture.view!.center = CGPoint(x: (gesture.view!.center.x) + translation.x,
+                               y: (gesture.view!.center.y) + translation.y)
+
+        gesture.setTranslation(.zero, in: boundaryView)
+
+    }
+    
+    func handleRotateGesture(_ gesture: UIRotationGestureRecognizer) {
+        
+    }
+
+    
+    func setupGesturesdOnImage(_ image: UIImage){
+        
+     
+        var custom_width = image.size.width
+        let height_to_width_ratio = image.size.height/image.size.width
+        if(image.size.width > boundaryView.bounds.size.width){
+            custom_width = boundaryView.bounds.size.width * 0.5
+        }
+        let custom_height =  height_to_width_ratio * custom_width
+        let frame = CGRect(x: 0 , y: (navigationController?.navigationBar.frame.height)! + 20 + 8, width: custom_width, height: custom_height)
+        
+        
+        let imageView = UIImageView(frame: frame)
+        
+        imageView.image = image
+        imageView.center = CGPoint(x: boundaryView.center.x, y: boundaryView.center.y)
+        imageView.layer.borderColor = UIColor.black.cgColor
+        imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(panGestureRecognizer)
+        //imageView.addGestureRecognizer(pinch_gesture)
+        imageView.addGestureRecognizer(rotationGestureRecognizer)
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+        imageView.layer.allowsEdgeAntialiasing = true
+
+        addImageToBoundarySubView(imageView)
+
+
+    }
+    
+    func addImageToBoundarySubView(_ imageView: UIImageView) {
+         boundaryView.addSubview(imageView)
     }
     
     func takePhotoFromCamera(){
@@ -88,6 +167,6 @@ extension CanvasViewController: UIImagePickerControllerDelegate, UINavigationCon
     }
     
     func updateImage(_ image: UIImage) {
-        
+        setupGesturesdOnImage(image)
     }
 }
