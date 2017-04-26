b8c3440d3590d01d026e79db66978890c3a1acdf
Salman Qureshi <er.salmanqureshi@gmail.com>
2017-04-26 06:06:04 +0530

    Implement image picker and add icons for gallery & camera

--- SQCanvas/SQCanvas/CanvasViewController.swift
+++ SQCanvas/SQCanvas/CanvasViewController.swift
@@ -26,6 +26,26 @@ class CanvasViewController: UIViewController {
     @IBAction func hanldeLauncher(_ sender: UIBarButtonItem) {
         bottomLauncher.showFloaterView()
     }
+    
+    func takePhotoFromCamera(){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func takeImageFromGallery(){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
 }
 
 extension CanvasViewController: BottomLauncherViewDelegate {
@@ -35,19 +55,39 @@ extension CanvasViewController: BottomLauncherViewDelegate {
             bottomLauncher.handleDismiss()
             
             print("Camera")
-            
+            takePhotoFromCamera()
             
         }else if index == 1{
             bottomLauncher.handleDismiss()
             
             print("Gallery")
-           
+            takeImageFromGallery()
+            
         }else{
             bottomLauncher.handleDismiss()
         }
-
     }
 }
 
-
-
+extension CanvasViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    //---------------ImagePicker Delegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [AnyHashable: Any]!)
+    {
+        let imgData = NSData(data: UIImageJPEGRepresentation((image), 1)!) as Data
+        
+        let imageSize: Int = imgData.count
+        print("Dimension width:-\(image.size.width) height:- \(image.size.height)\n---------size of image in KB: %f ", imageSize / 1024)
+        
+        updateImage(image)
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func updateImage(_ image: UIImage) {
+        
+    }
+}
